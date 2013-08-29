9c7fa5f7308d401ae7786ac867dab14f2ccc053e
Martin Bastien <martin.bastien@studiofrenetic.com>
2013-08-28 21:22:44 -0400

    fix config comment
    Signed-off-by: Martin Bastien <martin.bastien@studiofrenetic.com>

--- src/config/localization.php
+++ src/config/localization.php
@@ -7,9 +7,9 @@ return array(
 	| Your application locales
 	|--------------------------------------------------------------------------
 	|
-	| Your locale code is present as an array key, it can be in whatever your
+	| Your locale code is present as an array key, it can be in whichever your
 	| preferred format is. The value is the name of the language, it can be
-	| used for describing or displaying the name when switching locales.
+	| used for describing it or display the name when you switch locales.
 	|
 	*/
 	'locales' => array(
